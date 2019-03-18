51eda6af2427408a6a8e5cdb93c00db344997bc4
FabioZanon <fabio.zanon@bbz-sh.ch>
2019-03-18 08:04:12 +0100

    Zwischenstand

---
+++ script.js
@@ -0,0 +1,5 @@
+alert('Willkommen auf meiner Lerndokumentation-Website!');
+
+document.getElementById('button').onclick = function() {
+    alert("Durch die untenstehenden Links gelangen Sie zu meinen Lerndokumentationen");
+ }​;​
